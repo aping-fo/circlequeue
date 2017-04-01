3ae9657ff211797f947bda27d084dec6dee16273
aping.foo <125455364@qq.com>
2017-04-01 11:57:59 +0800

    readmd

--- CircleQueue.java
+++ CircleQueue.java
@@ -42,14 +42,6 @@ public class CircleQueue<E> extends AbstractQueue<E> {
         stampedLock = new StampedLock();
     }
 
-    /**
-     * Returns an iterator over the elements in this queue in proper sequence.
-     * The elements will be returned in order from first (head) to last (tail).
-     * <p>
-     * This iterator implementation NOT allow removes and co-modifications.
-     *
-     * @return an iterator over the elements in this queue in proper sequence
-     */
     @Override
     public Iterator<E> iterator() {
         return readConcurrently(() -> new Iter(headIndex, modificationsCount));
@@ -65,15 +57,6 @@ public class CircleQueue<E> extends AbstractQueue<E> {
         return size;
     }
 
-    /**
-     * Inserts the specified element at the tail of this queue if it is
-     * possible to do so immediately or if capacity limit is exited
-     * the oldest element (the head) will be evicted, and then the new element added at the tail.
-     * This method is generally preferable to method {@link #add},
-     * which can fail to insert an element only by throwing an exception.
-     *
-     * @throws NullPointerException if the specified element is null
-     */
     @Override
     public boolean offer(final E e) {
         requireNonNull(e, ILLEGAL_ELEMENT);
@@ -155,17 +138,6 @@ public class CircleQueue<E> extends AbstractQueue<E> {
         writeConcurrently(clearStrategy);
     }
 
-    /**
-     * Returns an array containing all of the elements in this queue, in
-     * proper sequence.
-     * <p>The returned array will be "safe" in that no references to it are
-     * maintained by this queue.  (In other words, this method must allocate
-     * a new array).  The caller is free to modify the returned array.
-     * <p>This method acts as bridge between array-based and collection-based
-     * APIs.
-     *
-     * @return an array containing all of the elements in this queue
-     */
     @Override
     public Object[] toArray() {
         if (size == 0) {
@@ -175,30 +147,6 @@ public class CircleQueue<E> extends AbstractQueue<E> {
         return destination;
     }
 
-    /**
-     * Returns an array containing all of the elements in this queue, in
-     * proper sequence; the runtime type of the returned array is that of
-     * the specified array.  If the queue fits in the specified array, it
-     * is returned therein.  Otherwise, a new array is allocated with the
-     * runtime type of the specified array and the size of this queue.
-     *
-     * <p>Like the {@link #toArray()} method, this method acts as bridge between
-     * array-based and collection-based APIs.  Further, this method allows
-     * precise control over the runtime type of the output array, and may,
-     * under certain circumstances, be used to save allocation costs.
-
-     * Note that {@code toArray(new Object[0])} is identical in function to
-     * {@code toArray()}.
-     *
-     * @param destination the array into which the elements of the queue are to
-     *          be stored, if it is big enough; otherwise, a new array of the
-     *          same runtime type is allocated for this purpose
-     * @return an array containing all of the elements in this queue
-     * @throws ArrayStoreException if the runtime type of the specified array
-     *         is not a supertype of the runtime type of every element in
-     *         this queue
-     * @throws NullPointerException if the specified array is null
-     */
     @Override
     @SuppressWarnings({"unchecked"})
     public <T> T[] toArray(final T[] destination) {
